a179d39c782261a5263b80a13005e69395ae59c6
Gradyn Wursten <me@gradyn.com>
2026-06-10 21:38:57 -0600

    try and fix resume flashing

--- src/apps/Resume.tsx
+++ src/apps/Resume.tsx
@@ -2,10 +2,21 @@ import type IApp from "./AppModel.ts";
 import {type AppProps} from "./AppModel.ts";
 import Icon from "../assets/img/windows98-icons/ico/certificate_2.ico";
 import LabelIcon from "../assets/img/agrorat.png"
+import "./Resume.css"
+import {useMemo} from "react";
 
 const Component = (props: AppProps) => {
+    // This is memoized to prevent the iframe from flashing for a re-render
+    const resumeFrame = useMemo(() => <iframe src="/resume.pdf" width="100%" height="100%" onLoad={() => console.log("resume load")}/>, []);
+
     return (
-        <iframe src="/resume.pdf" width="100%" height="100%"/>
+        <div style={{height: "100%"}}>
+            <div className="resume-url">
+                <input type="text" value="https://gradyn.com/resume.pdf"/>
+                <button onClick={() => window.open("https://gradyn.com/resume.pdf", "_blank")}>Open In Host</button>
+            </div>
+            {resumeFrame}
+        </div>
     )
 }
 
